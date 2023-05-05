6332279d156242354be9a424f12a7090443111f0
jcasadogp <juls.c99@gmail.com>
2023-05-05 17:11:06 +0200

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,95 @@
+<script>
+    import { page } from "$app/stores";
+    import "../app.css";
+  
+    export let data;
+    const { navigation, hierarchy } = data;
+    $: url = $page.url.pathname;
+    $: prev = navigation[url]?.prev;
+    $: up = navigation[url]?.up;
+    $: next = navigation[url]?.next;
+    $: children =
+      url === "/"
+        ? hierarchy
+        : url
+            .split("/")
+            .splice(1)
+            .reduce((agg, f) => agg[f], hierarchy);
+  </script>
+  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">
+      Data visualisation technologies - hands on
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        {#each Object.values(hierarchy).slice(2) as page}
+          <a
+            class="nav-item nav-link"
+            class:active={url.includes(page.url)}
+            href={page.url}
+          >
+            {page.name}
+          </a>
+        {/each}
+      </div>
+    </div>
+  </nav>
+  
+  <div class="container-fluid">
+    <slot />
+  
+    <!-- Contents -->
+    {#if Object.values(children).length > 2}
+      <ol>
+        {#each Object.values(children).splice(2) as page}
+          <li><a href={page.url}>{page.name}</a></li>
+        {/each}
+      </ol>
+    {/if}
+  
+    <!-- Navigation -->
+    <hr />
+    <div class="d-flex justify-content-between">
+      <a
+        type="button"
+        class="btn btn-light"
+        class:disabled={!prev}
+        href={prev ? prev.url : "/"}
+      >
+        ← {prev ? prev.name : ""}
+      </a>
+      <a
+        type="button"
+        class="btn btn-light"
+        class:disabled={!up || (prev && up.name == prev.name)}
+        href={up && (!prev || up.name != prev.name) ? up.url : "/"}
+      >
+        ↑ {up && (!prev || up.name != prev.name) ? up.name : ""}
+      </a>
+      <a
+        type="button"
+        class="btn btn-light"
+        class:disabled={!next}
+        href={next ? next.url : "/"}
+      >
+        {next ? next.name : ""} →
+      </a>
+    </div>
+  </div>
+  
+  <footer class="text-lg-start bg-light text-muted fixed-bottom">
+    (c) 2023 - Jelmer Bot, Jannes Peeters & Jan Aerts.
+  </footer>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<h1>Data visualisation technologies - Hands on</h1>
+<img src="/air_quality_cropped.jpeg" width="600px" alt="hero" />
+<p>
+  This website with exercises accompanies the material available at <a
+    class="link-primary"
+    href="https://vda-lab.gitlab.io/datavis-technologies"
+  >
+    https://vda-lab.gitlab.io/datavis-technologies
+  </a>. The material is licensed under the
+  <a href="https://creativecommons.org/licenses/by-nc/4.0/">
+    Creative Commons Attribution No-Commercial License
+  </a> CC-BY-NC: feel free to copy/remix/tweak/…​ it, but you need credit your source
+  (i.e. this website and its creators). It can also not be used for commercial 
+  purposes except by the original authors.
+</p>
+<img src="/ccbync.png" alt="ccby" /> (c) Jelmer Bot, Jan Aerts. 2022-2023
+
+<hr />
+
+<h2>Contents</h2>
+<!-- Child list and navigation added by layout -->
