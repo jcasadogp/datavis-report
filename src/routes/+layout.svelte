02f5351561298f117ed80af68b212444352d3ddc
jcasadogp <juls.c99@gmail.com>
2023-05-05 17:18:44 +0200

    organize project structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,95 +1,49 @@
 <script>
-    import { page } from "$app/stores";
-    import "../app.css";
-  
-    export let data;
-    const { navigation, hierarchy } = data;
-    $: url = $page.url.pathname;
-    $: prev = navigation[url]?.prev;
-    $: up = navigation[url]?.up;
-    $: next = navigation[url]?.next;
-    $: children =
-      url === "/"
-        ? hierarchy
-        : url
-            .split("/")
-            .splice(1)
-            .reduce((agg, f) => agg[f], hierarchy);
-  </script>
-  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">
-      Data visualisation technologies - hands on
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNavAltMarkup"
-      aria-controls="navbarNavAltMarkup"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        {#each Object.values(hierarchy).slice(2) as page}
-          <a
-            class="nav-item nav-link"
-            class:active={url.includes(page.url)}
-            href={page.url}
-          >
-            {page.name}
-          </a>
-        {/each}
-      </div>
-    </div>
-  </nav>
-  
-  <div class="container-fluid">
-    <slot />
-  
-    <!-- Contents -->
-    {#if Object.values(children).length > 2}
-      <ol>
-        {#each Object.values(children).splice(2) as page}
-          <li><a href={page.url}>{page.name}</a></li>
-        {/each}
-      </ol>
-    {/if}
-  
-    <!-- Navigation -->
-    <hr />
-    <div class="d-flex justify-content-between">
-      <a
-        type="button"
-        class="btn btn-light"
-        class:disabled={!prev}
-        href={prev ? prev.url : "/"}
-      >
-        ← {prev ? prev.name : ""}
-      </a>
-      <a
-        type="button"
-        class="btn btn-light"
-        class:disabled={!up || (prev && up.name == prev.name)}
-        href={up && (!prev || up.name != prev.name) ? up.url : "/"}
-      >
-        ↑ {up && (!prev || up.name != prev.name) ? up.name : ""}
-      </a>
-      <a
-        type="button"
-        class="btn btn-light"
-        class:disabled={!next}
-        href={next ? next.url : "/"}
-      >
-        {next ? next.name : ""} →
-      </a>
-    </div>
-  </div>
-  
-  <footer class="text-lg-start bg-light text-muted fixed-bottom">
-    (c) 2023 - Jelmer Bot, Jannes Peeters & Jan Aerts.
-  </footer>
-  
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
