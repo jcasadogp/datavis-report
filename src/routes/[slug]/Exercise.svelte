e4150bb1af5e02b3ddbfae7c43b6ca1ce7bade1b
jcasadogp <juls.c99@gmail.com>
2023-05-08 14:30:24 +0200

    changes in Exercise

--- src/routes/[slug]/Exercise.svelte
+++ src/routes/[slug]/Exercise.svelte
@@ -3,23 +3,35 @@
     import { min, max} from "d3-array";
     import { axisLeft, axisBottom } from 'd3-axis';
     import { select } from 'd3-selection';
-    import { each } from 'svelte/internal';
     import { schemeTableau10 } from "d3-scale-chromatic";
     
     // Properties
     export let gps_data_slug = undefined;
     export let carstops_data_slug = undefined;
-    // export let businesses_data_slug = undefined;
+    export let car_ids = undefined;
 
     export let cumulative_minute_total = 0;
-    let min_cum_min = min(gps_data_slug, d => d.cumulative_minute_total)
-    let max_cum_min = max(gps_data_slug, d => d.cumulative_minute_total)
 
     let hours = [0, 6, 12, 18, 24];
     // let days = [... new Set(gps_data_slug.map(d => d.day))].sort(function(a, b){return a - b});
     // let locations = [... new Set(carstops_data_slug.map(d => d.location.location_type))];
     // console.log(locations)
-    
+
+    let selected_car_id = gps_data_slug[0].car_id;
+    let previous_car_id;
+    let next_car_id;
+
+    if (car_ids.indexOf(selected_car_id)==0){
+        previous_car_id = selected_car_id;
+        next_car_id = car_ids[car_ids.indexOf(selected_car_id) +1]
+    } else if(car_ids.indexOf(selected_car_id)==car_ids.length-1){
+        previous_car_id = car_ids[car_ids.indexOf(selected_car_id) -1];
+        next_car_id = selected_car_id;
+    } else {
+        previous_car_id = car_ids[car_ids.indexOf(selected_car_id) -1];
+        next_car_id = car_ids[car_ids.indexOf(selected_car_id) +1]
+    }
+
     // Dimensions
     const width = 350;
     const height = 350;
@@ -35,7 +47,8 @@
     const latScale = scaleLinear().domain([min(gps_data_slug, d => d.lat), max(gps_data_slug, d => d.lat)]).range([innerHeight, 0]);
     const busScale = scaleOrdinal(schemeTableau10).domain(carstops_data_slug.map(b => b.location.location_type));
     
-    const timeScale = scaleLinear().domain([0, 86400]).range([0, innerWidth])//.ticks(hours) //seconds to width
+    const hourScale = scaleLinear().domain([0, 24]).range([0, innerWidth]) //seconds to width
+    const timeScale = scaleLinear().domain([0, 86400]).range([0, innerWidth]) //seconds to width
     const dayScale = scaleLinear().domain([6, 20]).range([0, innerHeight])
     
 
@@ -46,22 +59,36 @@
     }
 
     function bottomAxisBuilder(handle){
-        let axisGen = axisBottom(timeScale)
-        let xAxis = axisGen(select(handle))
+        let axisGen = axisBottom(hourScale)
+        let xAxis = axisGen.ticks(5).tickValues((hours))(select(handle))
+    }
+
+    $: get_cum_minutes_index = function(cmt) {
+        let current_day = Math.floor(cmt / 1440) + 6;
+        let current_second_day = (cmt % 1440) *60;
+        return [current_day, current_second_day]
     }
 
 </script>
 
-{#if gps_data_slug && carstops_data_slug}
+{#if gps_data_slug && carstops_data_slug && car_ids}
 
-<p><a href="/">Car overview</a></p>
+<table>
+    <tr>
+        <a href="/">Car overview</a>
+    </tr>
+    <tr>
+      <td><a href="{previous_car_id}">Previous car</a></td>
+      <td><a href="{next_car_id}">Next car</a></td>
+    </tr>
+</table>
 
+<h3>Details for car {selected_car_id}</h3>
 <div>
-<h3>Details for car XXX</h3>
-<input type="range" min={min_cum_min} max={max_cum_min} bind:value={cumulative_minute_total}> 
-<!-- {cumulative_minute_total} -->
+    <input type="range" min="0" max="20159" bind:value={cumulative_minute_total} width={innerWidth}>
 </div>
 
+<!-- Left part: map for chosen car id -->
 <svg width={width} height={height}>
     <g transform="translate({margin.left},{margin.top})">
         {#each gps_data_slug as gps_datapoint}
@@ -69,17 +96,18 @@
                 cx={longScale(gps_datapoint.long)} 
                 cy={latScale(gps_datapoint.lat)} 
                 r=3
-                fill="black"
-                fill-opacity="0.5"
+                class:car_window="{ Math.abs(gps_datapoint.cumulative_minute_total - cumulative_minute_total) < 15 }"
                 >
             </circle>
         {/each}
     </g>
 </svg>
 
-
+<!-- Right part: hours box -->
 <svg width={width} height={height}>
     <g transform="translate({margin.left},{margin.top})">
+        
+        <!-- Bar plot -->
         {#each carstops_data_slug as carstop, i}
             <rect 
                 x={timeScale(carstop.start.time) +1}
@@ -89,14 +117,52 @@
                 fill={busScale(carstop.location.location_type)}
                 />
         {/each}
+        
+        <!-- Left and bottom axes -->
         <g use:leftAxisBuilder></g>
         <g transform={`translate(0,${innerHeight})`} use:bottomAxisBuilder></g>
-        
-        <!-- {#each hours as hour}
-            <line x1={timeScale(hour*3600)} y1="0" x2={timeScale(hour * 3600)} y2={innerHeight} stroke="white" stroke-width="1" fill-opacity="1"/>
-            <text x={timeScale(hour*3600)} y={innerHeight +20} text-anchor="middle">{hour}</text>
-        {/each} -->
+
+        <!-- Moving bar -->
+        <line 
+            x1={timeScale(get_cum_minutes_index(cumulative_minute_total)[1])} 
+            x2={timeScale(get_cum_minutes_index(cumulative_minute_total)[1])}
+            y1={dayScale(get_cum_minutes_index(cumulative_minute_total)[0])}
+            y2={dayScale(get_cum_minutes_index(cumulative_minute_total)[0]) +dayHeight-2}
+            stroke="black" 
+            stroke-width="3" 
+            fill-opacity="1"/>
+
+        <!-- X axis legend bars -->
+        {#each hours as hour}
+            <line 
+                x1={hourScale(hour) +0.5}
+                x2={hourScale(hour) +0.5}
+                y1="0"
+                y2={innerHeight} 
+                stroke="black" 
+                stroke-width="1" 
+                fill-opacity="1"/>
+        {/each}
     </g>
 </svg>
 
 {/if}
+
+
+<style>
+    div{
+        width: 300px;
+        text-align: center;
+    }
+    input{
+        width: 300px;
+    }
+    circle{
+        fill: steelblue;
+        fill-opacity: 0.3;
+    }
+    circle.car_window{
+        fill: red;
+        fill-opacity: 1;
+    }
+</style>
